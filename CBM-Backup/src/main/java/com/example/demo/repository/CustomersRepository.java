d8e67cd324d46377dd0b41a6d1f17700de6c2aae
Saishree13 <144799043+Saishree13@users.noreply.github.com>
2023-10-04 14:10:07 +0530

    Delete CBM-Backup directory

--- CBM-Backup/src/main/java/com/example/demo/repository/CustomersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entities.Customers;
-
-public interface CustomersRepository extends JpaRepository<Customers, Integer>{
-
-}
